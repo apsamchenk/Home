1617f7ce2c1800f1927e061a4031a0fa42ff9d5d
Anna Samchenko <anna_ginz@list.ru>
2023-04-15 01:01:58 +0500

    Add Home5

---
+++ Home5/Home5/StudentsCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home5
+{
+    public class StudentsCreator
+    {
+        public Student[] CreateStudents(string[] names, string group)
+        {
+            Student[] arrayOfStudents = new Student[5];
+            Random random = new Random();
+            
+            for (int i = 0; i < arrayOfStudents.Length; i++)
+            {
+                int randId = random.Next(100);
+                int randMarkMath = random.Next(1, 11);
+                int randMarkPhEd = random.Next(1, 11);
+                int randMarkBiology = random.Next(1, 11);
+                int randAge = random.Next(18, 23);
+
+                arrayOfStudents[i] = new Student(randId, names[i], randAge, group, randMarkMath, randMarkPhEd, randMarkBiology, 0);
+            }
+
+            return arrayOfStudents;
+        }
+    }
+}
