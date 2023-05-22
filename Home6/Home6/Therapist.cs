70affc476ec957e9029f8488b0ef9ea1c5adc6c9
apsamchenk <129189673+apsamchenk@users.noreply.github.com>
2023-05-22 15:43:33 +0500

    Home work 6 (#5)

---
+++ Home6/Home6/Therapist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    internal class Therapist : Doctor
+    {
+        public Therapist (string name, int experience) : base(name, experience)
+        {
+
+        }
+
+        public override void Treat()
+        {
+            base.Treat();
+            Console.Write("other ilnesses");
+        }
+    }
+}
