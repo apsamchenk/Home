ac781d68aa899347f9a0cb5615d6f92780c36430
Anna Samchenko <anna_ginz@list.ru>
2023-05-22 16:01:09 +0500

    Add Home7

---
+++ Home7/Home7/Program.cs
@@ -0,0 +1,21 @@
+﻿using Home7;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cleaner cleaner = new Cleaner("Michael");
+            Cook cook = new Cook("Alex");
+            Manager manager = new Manager("Emma");
+
+            Worker[] workers = new Worker[] { cleaner, cook, manager };
+
+            Restaurant restaurant = new Restaurant("Omega", workers);
+
+            restaurant.Run(restaurant);
+        }
+    }
+}
---
+++ Home7/Home7/Restaurant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home7
+{
+    internal class Restaurant
+    {
+        public string Title { get; set; }
+
+        public Worker[] Workers { get; set; }
+
+        public Restaurant(string title, Worker[] workers)
+        {
+            Title = title;
+            Workers = workers;
+        }
+
+        public void Run(Restaurant restaurant) 
+        {
+            Console.WriteLine($"Restaurant title: {restaurant.Title}");
+
+            foreach (Worker worker in Workers) 
+            {
+                Work(worker);
+            }
+        }
+
+        public void Work(Worker worker)
+        {
+            if (worker is ICleanable cleaner)
+            {
+                cleaner.Clean();
+            }
+
+            if (worker is ICookable cook)
+            {
+                cook.FoodCook();
+            }
+
+            if (worker is IManageable manager)
+            {
+                manager.ManageWorkers();
+                manager.SolveProblems();
+            }
+        }
+    }
+}
