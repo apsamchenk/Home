17ad9ac2822c13ed4a95264c79ea5f27bb33ff73
apsamchenk <129189673+apsamchenk@users.noreply.github.com>
2023-06-12 00:42:06 +0500

    Add Hom9 (Exceptions and LINQ) (#7)

---
+++ Home9/Home9/AgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    public class AgeException : Exception
+    {
+        public AgeException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ Home9/Home9/SalaryException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    public class SalaryException : Exception
+    {
+        public SalaryException(string message) : base(message) 
+        { 
+
+        }
+    }
+}
