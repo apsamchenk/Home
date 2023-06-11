ea60190613245e0cbf22eba54373c9aa689d07d6
Anna Samchenko <anna_ginz@list.ru>
2023-06-12 00:39:33 +0500

    Add Hom9 (Exceptions and LINQ)

---
+++ Home9/Home9/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9
+{
+    public class Person
+    {
+        private int age, salary;
+        public string Name { get; set; }
+        public int Age { get { return age; }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new AgeException("Your age is below 18");
+                }
+
+                else 
+                {
+                    age = value;
+                }
+            }
+        }
+        public int Salary { get { return salary; }
+            set
+            {
+                if (value < 100)
+                {
+                    throw new SalaryException("The salary is below 100");
+                }
+
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public Person(string name, int age, int salary) 
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+    }
+}
---
+++ Home9/Home9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Home9 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            try 
+            {
+                Person person1 = new Person("Alex", 25, 1200);
+                Person person2 = new Person("Adam", 49, 1500);
+                Person person3 = new Person("Max", 28, 1900);
+                Person person4 = new Person("Alan", 29, 900);
+                Person person5 = new Person("Mary", 51, 1400);
+                Person person6 = new Person("Henry", 60, 1200);
+
+                persons.Add(person1);
+                persons.Add(person2);
+                persons.Add(person3);
+                persons.Add(person4);
+                persons.Add(person5);
+                persons.Add(person6);
+            }
+
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var peopleWithANames = persons.Where(person => person.Name.StartsWith('A')).Select(person => person.Name);
+
+            Console.WriteLine("People with names starts with A: ");
+
+            foreach (var people in peopleWithANames)
+            {
+                Console.WriteLine(people);
+            }
+
+            var peopleWithSalary = persons.Where(person => person.Age < 30 && person.Salary > 1000).Select(person => person.Name);
+
+            Console.WriteLine("\nPeople with Salary > 1000 and Age < 30: ");
+
+            foreach (var people in peopleWithSalary)
+            {
+                Console.WriteLine(people);
+            }
+
+            var firstPersonWithAgeMoreThen50 = persons.Where(person => person.Age > 50).Select(person => person.Name).First();
+
+            Console.WriteLine("\nFirst person with Age > 50: ");
+
+            Console.WriteLine(firstPersonWithAgeMoreThen50);
+        }
+    }
+}
